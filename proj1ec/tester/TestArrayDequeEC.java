a4881f5d2b74bd66cd80b74efb29d21ee62656ed
palospear <lyuxuan@wustl.edu>
2026-05-14 20:36:00 +0800

    up

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -3,6 +3,8 @@ package tester;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import edu.princeton.cs.algs4.StdRandom;
+import student.StudentArrayDeque;
+import student.ArrayDequeSolution;
 
 public class TestArrayDequeEC {
 
